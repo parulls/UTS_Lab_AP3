f5696530e1efda9bedb452b6d61cff35e16b3975
parulls <paruliandwireslia22@gmail.com>
2024-04-23 18:00:01 +0700

    mencari digit akhir yang tidak dikali dua

--- 032.cpp
+++ 032.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 main (){
+    system("cls");
     long long nomorKartu;
+    int digitAkhir;
     do 
     {
         cout<<"Masukkan nomor kartu : ";
@@ -9,4 +11,15 @@ main (){
     }
     while(nomorKartu<=0);
     cout<<"Nomor Kartu anda : "<<nomorKartu<<endl;
+    
+    long long angkaKartu=nomorKartu;
+    int checksum=0;
+    while(angkaKartu>0)
+    {
+        digitAkhir=angkaKartu%10;
+        checksum+=digitAkhir;
+        angkaKartu/=100;
+        cout<<digitAkhir<<endl;
+    }
+    
 }
